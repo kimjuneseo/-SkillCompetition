8fabce16f76df3aa934c6f38090bb85fea078928
kimjuneseo <june3929213512@gmail.com>
2021-02-16 22:00:03 +0900

    02-16

--- js/main.js
+++ js/main.js
@@ -1,24 +1,45 @@
-function slide() {
-    let pos = 0;
-    function play() {
-        pos = (pos + 1) % 3
-        $('.slide ul')
-            .animate({
-                marginTop: -300 * pos + "px"
-            }, 600)
-
-    }
-    setInterval(play, 3000)
-}
-
-$(document)
-    .on('ready', slide)
-
 window.onload = function () {
-    const cancel = document.querySelector('.cancel');
+    let count = 1
+    const cancelBtn = document.querySelector('.cancel');
+    const slideUl = document.querySelector('.slide li')
+    const slideCansl = document.querySelector('.slideCansle')
+    const slideGo = document.querySelector('.slideGo')
+    const nextBtn = document.querySelector('.next')
+    const backBtn = document.querySelector('.backWards')
 
-    cancel.addEventListener('click', function (e) {
+    function remove() {
         document.querySelector('.email').value = '';
         document.querySelector('.pass').value = '';
-    });
+    }
+    cancelBtn.addEventListener('click', remove)
+
+
+    function slide() {
+        for (let i = 0; i < count, count < 3; i++) {
+            slideUl.style.marginTop = -300 * count + "px"
+            slideUl.style.transition = 0.7 + "s"
+            count++
+            return
+        }
+        count = 0
+        function stop() {
+            clearInterval(set)
+        }
+
+        function go() {
+            setInterval(slide, 1500)
+        }
+        function next() {
+            count++
+        }
+        function backWards() {
+            count--
+        }
+        slideCansl.addEventListener('click', stop)
+        slideGo.addEventListener('click', go)
+        nextBtn.addEventListener('click', next)
+        backBtn.addEventListener('click', backWards)
+    }
+    let set = setInterval(slide, 1500)
+
 };
